17e09542a42b2f5c5d55de89cac726c3560f35ee
Abhijit Biswas <gamedev.abhijit@gmail.com>
2025-09-21 17:29:44 +0530

    added .gitignore,added CMakeLists, and tested the build

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include <stb/stb_image.h>
+#include <stb/stb_image_write.h>
+#include <raylib/raylib.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!";
+    return 0;
+}
