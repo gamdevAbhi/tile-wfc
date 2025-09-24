da5984518bfa0d2c01e3bb6d5079837d4c64b50f
Abhijit Biswas <gamedev.abhijit@gmail.com>
2025-09-24 14:25:32 +0530

    wfc algorithm working okay. need to fix the matching for neighbours as current it's taking the average of all boundaries.

--- include/wfc/tile_wfc.hpp
+++ include/wfc/tile_wfc.hpp
@@ -10,29 +10,27 @@
 
 namespace wfc
 {
-    struct Neighbour
-    {
-        wfc::eigenstate* eigenstate;
-        int noise;
-    };
-
     class tile_wfc 
     {
+        struct States
+        {
+            std::vector<int> possible_states;
+        };
+
         private:
         protected:
         int e_size;
         std::mt19937 gen;
-        std::map<wfc::eigenstate*, wfc::Neighbour*> eigenstate_map;
-
+        std::map<wfc::eigenstate*, int*> eigenstate_map;
+        eigenstate* get_eigenstate_at(int i);
+        int* get_lowest_possible_grid_element(States** grid, int row, int column);
+        void update_possible_grid_element(std::vector<int>* possible_eigenstate_index, eigenstate* neighbour_state, 
+        int max_allowed_noise);
+        void update_neighbours_possible(States** grid, int* results, int row, int column, eigenstate* state, int max_allowed_noise);
         int calculate_noise(wfc::eigenstate* state1, wfc::eigenstate* state2);
-        int get_lowest_superpos_index(std::vector<std::vector<int>> grid);
-        void remove_at(std::vector<std::vector<int>>* grid, int index);
-        void update_superpos(std::vector<int>* superpos, wfc::eigenstate* neighbour_state, int max_allowed_noise);
-        int update_grid_at(std::vector<std::vector<int>>* grid, int index);
-        wfc::eigenstate* get_eigenstate_at(int index);
         public:
         tile_wfc(wfc::eigenstate* eigenstates, int e_size);
-        wfc::eigenstate* generate(int row, int column, int max_allowed_noise = 30);
+        int** generate(int row, int column, int max_allowed_noise = 30);
     };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,66 @@
 #include <wfc/tile_wfc.hpp>
 #include <iostream>
 
-void generate_eigenstate(wfc::eigenstate* state, const char* path)
+Texture2D generate_eigenstate(wfc::eigenstate* state, const char* path)
 {
     int id, width, height, channels;
 
     unsigned char* data = stbi_load(path, &width, &height, &channels, 4);
 
+    if(!data)
+    {
+        std::cout << "image not loaded" << std::endl;
+    }
+
     *state = wfc::eigenstate(&id, data, width, height, channels);
+
+    Image image = { data, width, height, 1, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8};
+    
+    return LoadTextureFromImage(image);
 }
 
 int main()
 {
     InitWindow(600, 600, "TILE WFC");
 
+    int row = 20, column = 20, noise = 150;
     wfc::eigenstate* states = (wfc::eigenstate*)malloc(sizeof(wfc::eigenstate) * 5);
+    Texture2D textures[5];
 
-    generate_eigenstate(states, "../assets/images/1.png");
-    generate_eigenstate(states + 1, "../assets/images/2.png");
-    generate_eigenstate(states + 2, "../assets/images/3.png");
-    generate_eigenstate(states + 3, "../assets/images/4.png");
-    generate_eigenstate(states + 4, "../assets/images/5.png");
+    textures[0] = generate_eigenstate(states, "../assets/images/1.png");
+    textures[1] = generate_eigenstate(states + 1, "../assets/images/2.png");
+    textures[2] = generate_eigenstate(states + 2, "../assets/images/3.png");
+    textures[3] = generate_eigenstate(states + 3, "../assets/images/4.png");
+    textures[4] = generate_eigenstate(states + 4, "../assets/images/5.png");
+
+    std::cout << "generated eigenstate" << std::endl;
 
     wfc::tile_wfc wfc = wfc::tile_wfc(states, 5);
-    wfc.generate(4, 4);
 
-    // Image image = { data, width, height, 1, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8};
-    
-    // Texture2D tex = LoadTextureFromImage(image);
+    std::cout << "wfc generated" << std::endl;
+
+    int** grid = wfc.generate(row, column, noise);
+
+    std::cout << "grid generated" << std::endl;
 
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        for(int i = 0; i < row; i++)
+        {
+            int x_pixel = i * 16;
+
+            for(int j = 0; j < column; j++)
+            {
+                if((grid + i)[j] == nullptr) continue;
+                int y_pixel = j * 16;
+                
+                DrawTexture(textures[grid[i][j]], x_pixel, y_pixel, WHITE);
+            }
+        }
+
         // DrawTexture(tex, 0, 0, WHITE);
         EndDrawing();
     }
